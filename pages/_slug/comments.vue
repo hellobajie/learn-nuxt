8ab16826b42bd519c3014664715ae9ff48527420
王晓龙 <hello_bajie@163.com>
2018-10-26 16:57:32 +0800

    learn nuxt

---
+++ pages/_slug/comments.vue
@@ -0,0 +1,21 @@
+<template>
+    <section>
+        <div>我是_slug路径下面的comments</div>
+        <p>{{slug}}</p>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            slug: ''
+        }
+    },
+    mounted() {
+        console.log(this.$route);
+        this.slug = this.$route.params.slug;
+    },
+}
+</script>
+
---
+++ pages/user/_id.vue
@@ -0,0 +1,28 @@
+<template>
+    <section>
+        <div>我是user页，ID应该是可以动态传递的</div>
+        <div>{{id}}</div>
+        <p>我就是id，值为:{{id}}</p>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            id: 123
+        }
+    },
+    components: {},
+    mounted() {
+        console.log(this.$router);
+        console.log(this.$route);
+        console.log(this.$route.query);
+        console.log(this.$route.params.id);
+        this.id = this.$route.params.id;
+    },
+};
+</script>
+
+<style>
+</style>
